8edaf1c53fe90a20aa8115e041acc6ec63e4097a
Betty Seifu <betty.seifu149@gmail.com>
2017-08-14 10:36:02 +0300

    indentention correted

--- app.js
+++ app.js
@@ -4,11 +4,8 @@ xhr.send(null)
 xhr.onreadystatechange = function () {
   var DONE = 4; // readyState 4 means the request is done.
   var OK = 200; // status 200 is a successful return.
-  
-
-while (xhr.readyState === DONE&& xhr.status === OK) {
-  var res=JSON.parse(xhr.responseText);
-    
+  while (xhr.readyState === DONE&& xhr.status === OK) {
+      var res=JSON.parse(xhr.responseText);
       console.log(res); // 'This is the returned text.'
     } 
       console.log('Error: ' + xhr.status); // An error occurred during the request.
