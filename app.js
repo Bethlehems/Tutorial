14f0807e7908e6dda87abcafbd9cce519804d736
Betty Seifu <betty.seifu149@gmail.com>
2017-08-14 10:21:26 +0300

    Not loading

---
+++ app.js
@@ -0,0 +1,17 @@
+var xhr = new XMLHttpRequest();
+xhr.open('GET', '/data.json');
+xhr.send(null)
+xhr.onreadystatechange = function () {
+  var DONE = 4; // readyState 4 means the request is done.
+  var OK = 200; // status 200 is a successful return.
+  
+
+while (xhr.readyState === DONE&& xhr.status === OK) {
+  var res=JSON.parse(xhr.responseText);
+    
+      console.log(res); // 'This is the returned text.'
+    } 
+      console.log('Error: ' + xhr.status); // An error occurred during the request.
+    }
+
+
