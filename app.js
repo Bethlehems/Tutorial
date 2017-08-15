c05dc0b856f501143edaadd233ccda2ede8afc93
Betty Seifu <betty.seifu149@gmail.com>
2017-08-15 19:03:38 +0300

    table

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 var xhr = new XMLHttpRequest();
-xhr.open('GET', '/data.json');
-xhr.send(null)
-xhr.onreadystatechange = function () {
+xhr.open("GET", "/data.json");
+xhr.send(null);
+xhr.onreadystatechange = function() {
   var DONE = 4; // readyState 4 means the request is done.
   var OK = 200; // status 200 is a successful return.
   var res = JSON.parse(xhr.responseText);
-  var target = document.querySelector('#new');
-  var div = document.createElement('div');
-  while (xhr.readyState===DONE&&xhr.status === OK) {
-    div.innerHTML =res[1].first_name;
-    target.parentNode.after( div, target );
-    break;
-   } 
- console.log('Error: ' + xhr.status); // An error occurred during the request.
-};
 
- 
+  while (xhr.readyState === DONE && xhr.status === OK) {
+    var res = JSON.parse(xhr.responseText);
+    var target = document.querySelector("#new");
+    var txt, x;
+    txt += "<table border='1'>";
+    for (x in res) {
+      txt += "<tr>";
+      txt += "<td>" + res[x].first_name + "</tr>";
+      txt += "<td>" + res[x].last_name + "</tr>";
+      txt += "<td>" + res[x].gender + "</tr>";
+      txt += "</tr>";
+    }
+    txt += "</table>";
+    var div = document.createElement("div");
+    div.innerHTML = txt;
+    target.appendChild(div);
+
+    document.getElementById("new").style.color = "red";
+
+    break;
+  }
+  console.log("Error: " + xhr.status); // An error occurred during the request.
+};
