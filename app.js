870178b4d8d42ff88c26d44d08819dcf065f4f1b
Betty Seifu <betty.seifu149@gmail.com>
2017-08-15 16:15:05 +0300

    div added.

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ xhr.send(null)
 xhr.onreadystatechange = function () {
   var DONE = 4; // readyState 4 means the request is done.
   var OK = 200; // status 200 is a successful return.
-  while (xhr.readyState === DONE&& xhr.status === OK) {
-      var res=JSON.parse(xhr.responseText);
-      console.log(res); // 'This is the returned text.'
+  var res = JSON.parse(xhr.responseText);
+ var target = document.querySelector('#new');
+var div = document.createElement('div');
+var name=document.getElementById('first_name');
+  while (xhr.readyState===DONE&&xhr.status === OK) {
+  
+    div.innerHTML =name;
+    target.parentNode.after( div, target );
+    break;
+  
     } 
-      console.log('Error: ' + xhr.status); // An error occurred during the request.
-    }
+ console.log('Error: ' + xhr.status); // An error occurred during the request.
+};
 
-
+ 
